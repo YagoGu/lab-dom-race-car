93cc5d46c2e7a25c9eb28c9d857320ac39a18125
YagoGu <yago.gutierrez15@gmail.com>
2023-07-20 17:50:42 +0200

    Iteration 5

--- js/script.js
+++ js/script.js
@@ -1,14 +1,33 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
-
+  let game;
   startButton.addEventListener("click", function () {
     startGame();
   });
 
   function startGame() {
     console.log("start game");
-    const game = new Game();
+    game = new Game();
     game.start()
   }
+
+  function handleKeyBoardInput (key) {
+    if (key === "ArrowUp") {
+      game.player.directionY = -1;
+    }
+    else if (key === "ArrowDown") {
+      game.player.directionY = 1;
+    }
+    else if (key === "ArrowLeft") {
+      game.player.directionX = -1;
+    }
+    else if (key === "ArrowRight") {
+      game.player.directionX = 1;
+    }
+  }
+
+  window.addEventListener("keydown", (event) => {
+    handleKeyBoardInput (event.key);
+  })
 };
